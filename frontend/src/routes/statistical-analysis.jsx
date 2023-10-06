c0a498b1cfc268e19edb18652b83e14ce79f3a65
Hugo <A01177091@itesm.mx>
2023-10-05 18:46:18 -0600

    dark theme added

--- frontend/src/routes/statistical-analysis.jsx
+++ frontend/src/routes/statistical-analysis.jsx
@@ -21,42 +21,48 @@ export default function StatisticalAnalysis() {
   
 
   return (
-    <div style={{ padding: '24px', backgroundColor: '#080808', minHeight: '100vh' }}>
-      <Title style={{ color: '#c8e8e6' }} level={1}>Statistics</Title>
+    <div style={{ padding: '24px', backgroundColor: '#0d0f11', minHeight: '100vh' }}>
+      <Title style={{ color: 'white' }} level={1}>Statistics</Title>
       <Text style={{ color: 'white', marginBottom: '2rem' }}>Basic statistical values for CSV data</Text>
 
       {Object.entries(allStats).map(([filename, stats]) => (
-        <Card title={filename} key={filename} style={{ marginBottom: '24px', backgroundColor: '#919191', color: 'white' }}>
+        <Card key={filename} title={filename} style={{ marginBottom: '24px', backgroundColor: '#0d0f11', color: 'white' }}>
           {typeof stats === 'object' && !Array.isArray(stats) ? (
-            Object.entries(stats).map(([column, values]) => (
-              <div key={column}>
-                <Title style={{ color: '#ffffff' }} level={3}>{column}</Title>
-                <Table
-                  dataSource={[{ key: 'mean', value: 50 }, { key: 'median', value: 60 }]}
-                  columns={[
-                    {
-                      title: 'Stat',
-                      dataIndex: 'key',
-                      key: 'key',
-                      render: text => <Text style={{ color: 'black' }}>{text}</Text>
-                    },
-                    {
-                      title: 'Value',
-                      dataIndex: 'value',
-                      key: 'value',
-                      render: text => <Text style={{ color: 'black' }}>{text}</Text>
-                    }
-                  ]}
-                  pagination={false}
-                  style={{ backgroundColor: '#292929' }}
-                />
-              </div>
-            ))
+            Object.entries(stats).map(([column, values]) => {
+              const tableData = Object.entries(values).map(([key, value]) => ({ key, value }));
+              return (
+                <div key={column}>
+                  <Title style={{ color: '#ffffff' }} level={3}>{column}</Title>
+
+                  <Table
+                    pagination={false}
+                    style={{ backgroundColor: '#292929' }}
+                    rowClassName={()=>'row-style'}
+                    dataSource={tableData}
+                    columns={[
+                      {
+                        title: 'Stat',
+                        dataIndex: 'key',
+                        key: 'key',
+                        render: text => <Text style={{ color: '#ffffff' }}>{text}</Text>
+                      },
+                      {
+                        title: 'Value',
+                        dataIndex: 'value',
+                        key: 'value',
+                        render: text => <Text style={{ color: '#ffffff' }}>{text}</Text>
+                      }
+                    ]}
+                  />
+                </div>
+              );
+            })
           ) : (
-            <Text style={{ color: 'black' }}>{stats}</Text>
+            <Text style={{ color: '#ffffff' }}>{stats}</Text>
           )}
         </Card>
       ))}
     </div>
   );
 }
+
