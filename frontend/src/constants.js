1a401f2ba73e3565c7c94d0141e004794b601bcf
David Hernandez <d.hdza19@gmail.com>
2023-11-14 00:16:10 -0600

    add constants for styling

---
+++ frontend/src/constants.js
@@ -0,0 +1,18 @@
+export const COLORS = {
+  white: "hsl(0deg 0% 100%)",
+  gray: {
+    100: "hsl(185deg 5% 95%)",
+    300: "hsl(190deg 5% 80%)",
+    500: "hsl(196deg 4% 60%)",
+    600: "hsl(212deg 8% 68%)",
+    700: "hsl(220deg 5% 40%)",
+    900: "hsl(220deg 3% 20%)",
+  },
+  primary: "hsl(218deg 93% 58%)",
+};
+
+export const WEIGHTS = {
+  normal: 500,
+  medium: 600,
+  bold: 800,
+};
